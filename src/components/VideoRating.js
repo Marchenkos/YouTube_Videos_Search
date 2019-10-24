0d1e4fd10a89ce9d1bc968b09323a1a1a4a49748
Kseniya Marchanka <Kseniya.Marchanka@itechart-group.com>
2019-10-24 18:27:53 +0300

    Start mobile version

--- src/components/VideoRating.js
+++ src/components/VideoRating.js
@@ -2,24 +2,34 @@ import React from "react";
 import "../style/rating-information.less";
 
 export default function VideoRating({ value: { viewCount, commentCount, likeCount } }) {
+    const numbFormat = numb => {
+        if (numb > 1000000) {
+            return `${(numb / 1000000).toFixed(1)} ml`;
+        } else if (numb > 1000) {
+            return `${Math.floor(numb / 1000)} th`;
+        }
+
+        return numb;
+    };
+
     return (
         <div className="rating-information">
-            <div className="rating-information__likes">
+            <div className="rating-information__block">
                 <span className="icon-heart item__icon" />
                 <p className="item__count">
-                    {likeCount}
+                    {numbFormat(likeCount)}
                 </p>
             </div>
-            <div className="rating-information__views">
+            <div className="rating-information__block">
                 <span className="icon-eye item__icon" />
                 <p className="item__count">
-                    {viewCount}
+                    {numbFormat(viewCount)}
                 </p>
             </div>
-            <div className="rating-information__comments">
+            <div className="rating-information__block">
                 <span className="icon-bubbles2 item__icon" />
                 <p className="item__count">
-                    {commentCount}
+                    {numbFormat(commentCount)}
                 </p>
             </div>
         </div>
