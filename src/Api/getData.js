919ba6a0fe7a8bf3bdf185accfe48b3c14b2d4f7
Ksenia <43186251+Marchenkos@users.noreply.github.com>
2019-11-04 14:17:30 +0300

    Mobile version for video-components (#7)

--- src/Api/getData.js
+++ src/Api/getData.js
@@ -67,7 +67,7 @@ function searchVideo(keyword, nextPage = "") {
 }
 
 export default function loadClient(keyword, nextPageToken, onSuccess, onError) {
-    gapi.client.setApiKey("AIzaSyDubE0IDSRQvaJlnDVOvkzH8WDXH_MtZ5E");
+    gapi.client.setApiKey("AIzaSyA9MPN_HJzdeH7MzqAP8-9LE35Ch3HHBs0");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(() => { console.log("GAPI client loaded for API"); })
         .then(() => {
@@ -88,5 +88,5 @@ export default function loadClient(keyword, nextPageToken, onSuccess, onError) {
 }
 
 gapi.load("client", () => {
-    gapi.client.init({ apiKey: "AIzaSyDubE0IDSRQvaJlnDVOvkzH8WDXH_MtZ5E" });
+    gapi.client.init({ apiKey: "AIzaSyA9MPN_HJzdeH7MzqAP8-9LE35Ch3HHBs0" });
 });
